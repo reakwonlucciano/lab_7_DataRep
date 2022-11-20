8323987803104d4d4ee0f65ae5f9aa21d5909572
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-11-20 18:24:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap navigation bar
 import './App.css';
-import { Content } from './components/content';
-import { Footer } from './components/footer';
-import { Header } from './components/header';
-import Navbar from 'react-bootstrap/Navbar';
+import { Content } from './components/content'; // import middle content
+import { Footer } from './components/footer';  // import footer
+import { Header } from './components/header';  // import header
+import Navbar from 'react-bootstrap/Navbar'; // import nav bar
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import {Read} from './components/read';
-import { Create } from './components/create';
+import Nav from 'react-bootstrap/Nav'; // import nav bar
+
+import {Read} from './components/read'; // import for read
+import { Create } from './components/create';  // import for create class
 import {
   BrowserRouter as Router,
   Routes,
   Route
-} from 'react-router-dom';
+} from 'react-router-dom'; // gives router and switch
 
+
+// imports class from react called conponent/ this the main component
 class App extends React.Component {
+   // method that takes no arguements
   render() {
+    
+    // headers and time 
+    // header, content and footer being imported
+    // imported bootstrap using nav bar code given on site
+
     return (
       <Router>
       <div className="App">
@@ -41,7 +50,7 @@ class App extends React.Component {
       </div>
       </Router>
     );
-  }
-}
+  } //render
+} //class app
 
 export default App;
